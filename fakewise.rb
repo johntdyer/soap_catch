285a5e3288fb1c99948e0b5377f611d842e79de8
John Dyer <john@krumpt.com>
2011-04-25 21:09:35 +0000

    removed get route, no longer needed

--- fakewise.rb
+++ fakewise.rb
@@ -12,11 +12,3 @@ post '/' do
   
 '<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><ns:uploadUsageEventsResponse xmlns:ns="http://connectsm.ws.bwse.com/xsd"><ns:return>Success</ns:return></ns:uploadUsageEventsResponse></soapenv:Body></soapenv:Envelope>'
 end
-
-get '/' do
-  $LOG.info(request.env["rack.input"].read)
-  $LOG.debug(params[:splat])
-  content_type 'text/xml', :charset => 'utf-8'
-'<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><ns:uploadUsageEventsResponse xmlns:ns="http://connectsm.ws.bwse.com/xsd"><ns:return>Success</ns:return></ns:uploadUsageEventsResponse></soapenv:Body></soapenv:Envelope>'  
-
-end
